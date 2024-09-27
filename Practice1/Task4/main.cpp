8732445e979449d7c1d3c3a30461a73f1f744d6d
Yaroslav Oslam <ggStrider1@gmail.com>
2024-09-28 00:57:06 +0300

    Completed Task 4 (practice 1)

---
+++ Practice1/Task4/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int age, salary;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "Enter your salary: ";
+    cin >> salary;
+
+    if(!(age > 20 && salary > 400 && salary < 1000)) {
+        cout << "You are not suitable" << endl;
+        return 0;
+    }
+
+    cout << "We will consider your position" << endl;
+
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+
+    string desiredPosition;
+    cout << "Enter your desired position: ";
+    cin >> desiredPosition;
+
+    cout << endl << "Your salary: " << salary << endl << "Your age: " << age << endl;
+    cout << "Your name: " << name << endl << "Your desired position: " << desiredPosition << endl;
+}
